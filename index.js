abcac296016001bd1abe728d57bfda7ae0d41a74
nyinyi979 <nyinyi095062687@gmail.com>
2025-02-05 19:49:41 +0700

    typescript configuration

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-// Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true })
-
-// Declare a route
-fastify.get('/', function handler (request, reply) {
-  reply.send({ hello: 'world' })
-})
-
-// Run the server!
-fastify.listen({ port: 3000 }, (err) => {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-})
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fastify_1 = __importDefault(require("fastify"));
+const server = (0, fastify_1.default)();
+server.get('/ping', async (request, reply) => {
+    return 'pong\n';
+});
+server.listen({ port: 8080 }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+});
